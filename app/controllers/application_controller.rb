69da23792ee5dd9df9db02f5fc868796a6a53931
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-05 19:30:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,8 +45,12 @@ class ApplicationController < Sinatra::Base
     @article.title = params[:title]
     @article.content = params[:content]
     @article.save
-    
+
     redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    
   end
 
 end
